b33e3c920ec1efe4c8b35f1049aafcee0fdae4c9
Mathieu Comandon <strycore@gmail.com>
2017-10-14 01:19:03 -0700

    Add support for random Lutris games

--- bot.js
+++ bot.js
@@ -58,33 +58,105 @@ bot.on("message", function (user, userID, channelID, message, evt) {
 	}
 
 	function searchLutris(args) {
-		var qs = args.join("+");
+		var qs;
+		if (args[0] == 'random') {
+			qs = 'random=' + args[1]
+			responseHandler = handleLutrisRandom;
+		} else {
+			qs = 'search=' + args.join("+");
+			responseHandler = handleLutrisSearch;
+		}
 		request.get({
-			url: "https://lutris.net/api/games?search=" + qs,
+			url: "https://lutris.net/api/games?" + qs,
 			json: true
-		},
-		function(e, r, data) {
-// 			logger.info("search data: " + JSON.stringify(data));
-			var results = data["results"];
-			var loops = (results.length > 10) ? 10 : results.length;
-			var msg = "";
-			if(loops == 0)
-				msg = "Search returned 0 results";
-			else {
-				msg = "First " + loops + " on Lutris:\n```"
-				for(var i = 0; i < loops; i++) {
-					var row = results[i];
-	// 				if(!row)
-	// 					logger.info("i: " + i + " loops: " + loops);
-	// 				logger.info("row: " + JSON.stringify(row));
-					msg += (i + 1) + " - " + row["name"] + "\n";
-				}
-				msg += "```";
+		}, responseHandler);
+	}
+
+	function handleLutrisRandom(e, r, data) {
+		var results = data['results'];
+		if (!results) {
+			return;
+		}
+		var game = results[0];
+		var fields = []
+		if (game.year) {
+			fields.push({
+				name: "Year",
+				value: game.year,
+				inline: true
+			})
+		}
+		if (game.platforms.length) {
+			platforms = []
+			for (var i=0; i< game.platforms.length; i++) {
+				platforms.push(game.platforms[i].name)
 			}
-			setTimeout(function() {
-				bot.sendMessage({ to: channelID, message: msg });
+			fields.push({
+				name: "Platforms",
+				value: platforms.join(','),
+				inline: true
+			})
+		}
+		var author;
+		if (game.steamid) {
+			 author = {
+				"name": "Available on Steam",
+				"url": "http://store.steampowered.com/app/" + game.steamid + "/",
+				"icon_url": "https://lutris.net/static/images/icons/steam.png"
+			}
+		} else {
+			author = {};
+		}
+
+		var footer;
+		footer = {
+				text: ""
+		}
+
+		setTimeout(function() {
+			var res = bot.sendMessage({
+				to: channelID,
+				message: '',
+				url: 'https://lutris.net/games/' + game['slug'],
+				embed: {
+					color: 0xf89a15,
+					title: game['name'],
+					thumbnail: {
+						url: "https://lutris.net/static/images/logo.png"
+					},
+					author: author,
+					footer: footer,
+					fields: fields,
+					image: {
+						url: 'https://lutris.net' + game['banner_url']
+					}
+				}
 			}, 0);
+			console.log(res);
 		});
+	}
+
+	function handleLutrisSearch(e, r, data) {
+	//		logger.info("search data: " + JSON.stringify(data));
+		var results = data["results"];
+		var loops = (results.length > 10) ? 10 : results.length;
+		var msg = "";
+		if(loops == 0)
+			msg = "Search returned 0 results";
+		else {
+			msg = "First " + loops + " on Lutris:\n```"
+			for(var i = 0; i < loops; i++) {
+				var row = results[i];
+				// if(!row)
+				//	logger.info("i: " + i + " loops: " + loops);
+				//	logger.info("row: " + JSON.stringify(row));
+				msg += (i + 1) + " - " + row["name"] + "\n";
+			}
+			msg += "```";
+		}
+		setTimeout(function() {
+			bot.sendMessage({ to: channelID, message: msg });
+		}, 0);
 	}
 
 	function makeInsult(name) {
